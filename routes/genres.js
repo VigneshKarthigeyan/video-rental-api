de547192f421842aff5df7067298a3723f672305
VigneshKarthigeyan <vickykarthy123@gmail.com>
2022-02-07 23:55:36 +0530

    routes/genres.js is updated with async middleware

--- routes/genres.js
+++ routes/genres.js
@@ -1,16 +1,17 @@
 const {Genre, validate} = require('../models/genre');
 const mongoose = require('mongoose');
+const asyncMiddleware=require('../middleware/async-errors')
 const express = require('express');
 const router = express.Router();
 const auth=require('../middleware/auth');
 const role=require('../middleware/role');
 
-router.get('/', async (req, res) => {
-  const genres = await Genre.find().sort('name');
+router.get('/', asyncMiddleware(async (req, res) => {
+  const genres = await Genre.find().sort('poiy');
   res.send(genres);
-});
+}));
 
-router.post('/',auth, async (req, res) => {
+router.post('/',auth, asyncMiddleware(async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -18,9 +19,9 @@ router.post('/',auth, async (req, res) => {
   genre = await genre.save();
   
   res.send(genre);
-});
+}));
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', asyncMiddleware(async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -31,22 +32,22 @@ router.put('/:id', async (req, res) => {
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
   
   res.send(genre);
-});
+}));
 
-router.delete('/:id',[auth,role], async (req, res) => {
+router.delete('/:id',[auth,role], asyncMiddleware(async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
   res.send(genre);
-});
+}));
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', asyncMiddleware(async (req, res) => {
   const genre = await Genre.findById(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
   res.send(genre);
-});
+}));
 
 module.exports = router;
